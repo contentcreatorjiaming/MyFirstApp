ff70aeb4ac67d2ccd4835c32f604b6d4b5b771de
Jiaming <rhymingjiaming@gmail.com>
2026-07-12 17:35:10 -0500

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Jiaming Lou on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
